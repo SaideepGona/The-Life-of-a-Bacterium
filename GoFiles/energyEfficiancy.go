06b66631d43e56814a371763b30262a0ce54bf33
Saideep Gona <gona.saideep1@gmail.com>
2017-11-23 21:01:25 -0500

    energy efficiency still with BurnEnergy

--- GoFiles/energyEfficiancy.go
+++ GoFiles/energyEfficiancy.go
@@ -1,6 +1,8 @@
 // Saideep Gona
 // Methods related to energy efficiancy
 
+package main
+
 func (dna *DNA) UpdateEE() {
 
 	phenotype = "Energy Efficiency"
@@ -10,4 +12,18 @@ func (dna *DNA) UpdateEE() {
 
 }
 
-func (bact *Bacteria) BurnEnergy()
+func (bact *Bacteria) BurnEnergy(phenotype string) {
+
+	if phenotype == "movement" {
+		phenVal := bact.movement
+		scale := 1.0
+	}
+
+	if phenotype == "antibiotic" {
+		phenVal := bact.potency
+		scale := 1.0
+	}
+
+	burnAmount := bact.energyEfficiancy * phenVal * scale
+	bact.currentEnergy = bact.currentEnergy - burnAmount
+}
